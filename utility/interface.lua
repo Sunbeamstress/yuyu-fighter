6d77e2f1f427319d21acf921bc50c98eea18278f
Sunbeamstress <brendon.hollesen@gmail.com>
2026-02-16 13:43:01 -0700

    UI polish, converted hard coded format to table based

--- utility/interface.lua
+++ utility/interface.lua
@@ -4,34 +4,77 @@ ui.panel_color = {0.15, 0.18, 0.24}
 
 ui.left = {
     name = {
-        x = 8, y = 2,
+        x = 8,
+        y = 2,
         color = {0.9, 0.9, 0.9},
         value = function ()
             return "Test Guy" -- this should derive from the character's name!
         end,
     },
     health = {
-        x = 70, y = 4,
-        w = 200, h = 12,
+        x = 70,
+        y = 4,
+        w = 200,
+        h = 12,
         value = function ()
-            return 100 * (game.fighter.two.health / 100)
+            return 100 * (game.fighter.one.health / 100)
         end
+    },
+    accuracy = {
+        x = 8,
+        y = game.window.height - 50,
+        w = 200,
+        h = 10,
+        color = {0.5, 0.5, 0.7},
+        value = function ()
+            return 100 * (game.fighter.one.accuracy / 100)
+        end
+    },
+    text = {
+        x = 8,
+        y = game.window.height - 70,
+        value = function ()
+            return game.fighter.one.text
+        end,
     },
 }
 
+
+
 ui.right = {
     name = {
-        x = 8, y = 2,
+        x = game.window.width - 64,
+        y = 2,
         color = {0.9, 0.9, 0.9},
         value = function ()
             return "Test Guy" -- this should derive from the character's name!
         end,
     },
     health = {
-        x = -70, y = 4, w = 200, h = 12,
+        x = game.window.width - 270,
+        y = 4,
+        w = 200,
+        h = 12,
         value = function ()
             return 100 * (game.fighter.two.health / 100)
         end
+    },
+    accuracy = {
+        x = game.window.width - 208,
+        y = game.window.height - 50,
+        w = 200,
+        h = 10,
+        color = {0.5, 0.5, 0.7},
+        value = function ()
+            return 100 * (game.fighter.two.accuracy / 100)
+        end
+    },
+    text = {
+        x = game.window.width - 208,
+        y = game.window.height - 70,
+        value = function ()
+            return game.fighter.two.text
+        end,
     },
 }
 
@@ -39,7 +82,7 @@ ui.right = {
 
 
 function draw_interface()
-    local x, y, w, h
+    local x, y, w, h, v, str, adj
     local panel_color = {0.15, 0.18, 0.24}
 
 
@@ -50,65 +93,80 @@ function draw_interface()
     set_color(unpack(panel_color))
     love.graphics.rectangle("fill", 0, 0, game.window.width, 20)
 
-    -- TOP PANEL: Character names
-    -- for _, dir in ipairs({"left", "right"}) do
-    --     local d_tbl = ui[dir]
-    --     set_color()
-    -- end
+    for _, dir in ipairs({"left", "right"}) do
+        local d_tbl = ui[dir]
 
-    set_color(0.9)
-    love.graphics.print(game.character.testguy.name, 8, 2) -- P1
-    set_color(0.9)
-    love.graphics.print(game.character.testguy.name, game.window.width - 64, 2) -- P2
+        -- TOP PANEL: Character Names
+        set_color(unpack(d_tbl.name.color))
+        x = d_tbl.name.x
+        y = d_tbl.name.y
+        str = d_tbl.name.value()
+        love.graphics.print(str, x, y)
 
-    -- TOP PANEL: Health Bars
-    set_color(0)             -- background
-    love.graphics.rectangle("fill", 70, 4, 200, 12) -- P1
-    love.graphics.rectangle("fill", game.window.width - 270, 4, 200, 12) -- P2
+        -- TOP PANEL: Health Bars
+        w = d_tbl.health.w
+        h = d_tbl.health.h
+        v = d_tbl.health.value()
 
-    set_color(0.8, 0.1, 0.1) -- foreground
-    w = 200 * (game.fighter.one.health / 100)
-    x = 70
-    love.graphics.rectangle("fill", x, 4, w, 12) -- P1
-    w = 200 * (game.fighter.two.health / 100)
-    x = (game.window.width - 270) + (200 - w)
-    love.graphics.rectangle("fill", x, 4, w, 12) -- P2
+        x = d_tbl.health.x
+        y = d_tbl.health.y
+
+        set_color(0) -- background
+        love.graphics.rectangle("fill", x, y, w, h)
+
+        adj = 200 * (v / 100)
+        if dir == "right" then
+            x = x + (w - adj)
+        end
+        w = adj
+        set_color(0.8, 0.1, 0.1)
+        love.graphics.rectangle("fill", x, y, w, h)
+    end
 
 
 
     -- BOTTOM PANEL ------------------------------------------------------------
-    x = 0
-    y = game.window.height - 60
-    w = game.window.width
-    h = 60
 
     -- BOTTOM PANEL: Background
     set_color(unpack(panel_color))
-    love.graphics.rectangle("fill", x, y, w, h)
+    love.graphics.rectangle("fill", 0, game.window.height - 60, game.window.width, 60)
 
     -- BOTTOM PANEL: Accuracy Bars
     x = 8
     y = game.window.height - 50
     w = 200
     h = 10
-    set_color(0)             -- background
-    love.graphics.rectangle("fill", x, y, w, h) -- P1
-    love.graphics.rectangle("fill", game.window.width - (208), y, w, h) -- P2
-    w = 200 * (game.fighter.one.accuracy / 100)
-    set_color(0.5, 0.5, 0.7) -- foreground
-    love.graphics.rectangle("fill", x, y, w, h) -- P1
-    w = 200 * (game.fighter.two.accuracy / 100)
-    x = (game.window.width - 208) + (200 - w)
-    love.graphics.rectangle("fill", x, y, w, h) -- P2
 
+    for _, dir in ipairs({"left", "right"}) do
+        local d_tbl = ui[dir]
 
+        -- BOTTOM PANEL: Accuracy Bars
+        x = d_tbl.accuracy.x
+        y = d_tbl.accuracy.y
+        w = d_tbl.accuracy.w
+        h = d_tbl.accuracy.h
+        v = d_tbl.accuracy.value()
 
-    -- TEXT --------------------------------------------------------------------
-    love.graphics.setColor( 1, 1, 1, 1 )
-    love.graphics.print(game.fighter.one.text, 0, 30, 0, 1, 1)
-    love.graphics.print(game.fighter.two.text, 300, 30, 0, 1, 1)
-    love.graphics.print("P1 Accuracy = %s" % {game.fighter.one.accuracy}, 0, 300, 0, 1, 1)
-    love.graphics.print("P2 Accuracy = %s" % {game.fighter.two.accuracy}, 300, 300, 0, 1, 1)
+        set_color(0) -- background
+        love.graphics.rectangle("fill", x, y, w, h)
+
+        set_color(unpack(d_tbl.accuracy.color)) -- foreground
+        adj = 200 * (v / 100)
+        if dir == "right" then
+            x = x + (w - adj)
+        end
+        w = adj
+        love.graphics.rectangle("fill", x, y, w, h)
+
+        -- text indicators
+        x = d_tbl.text.x
+        y = d_tbl.text.y
+        str = d_tbl.text.value()
+
+        set_color(0.9, 0.8, 0.7)
+        love.graphics.print(str, x, y + 4)
+        love.graphics.print("%s player Accuracy: %d%%" % {dir:title(), v}, x, y + 32)
+    end
 
 
 
