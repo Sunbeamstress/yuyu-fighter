79998200870d0b7b716ad34b9539a147bc824d90
Sunbeamstress <brendon.hollesen@gmail.com>
2026-02-16 11:33:11 -0700

    added fast quit and restart keys for debugging

--- utility/_control.lua
+++ utility/_control.lua
@@ -1,5 +1,13 @@
 function love.keypressed(key, code, rep)
+    if key:lower() == "q" then
+        love.event.quit()
+        return
+    end
 
+    if key:lower() == "r" then
+        love.event.push("quit", "restart")
+        return
+    end
 end
 
 
