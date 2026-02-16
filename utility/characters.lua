38490c7e15a803344f22898a711f8f81ed4a46cf
Sunbeamstress <brendon.hollesen@gmail.com>
2026-02-16 12:19:29 -0700

    some rudimentary character definition

--- utility/characters.lua
+++ utility/characters.lua
@@ -1,1 +1,26 @@
 game.character = {}
+
+
+
+game.character.testguy = {}
+game.character.testguy.name = "Test Guy"
+game.character.testguy.max_hp = 100
+game.character.testguy.speed = 1.0
+game.character.testguy.victory_lines = {
+    "You must defeat Shen Long to stand a chance!",
+    "Go home and be a family man!",
+    "aww man bro are u ok i didnt mean to hit u that hard fuck"
+}
+
+
+
+game.character.pilot = {}
+game.character.pilot.name = "The Pilot"
+game.character.pilot.max_hp = 90
+game.character.pilot.speed = 1.2
+game.character.pilot.victory_lines = {
+    "We can rebuild the world without you.",
+    "I must have been imagining them...",
+    "Another notch for Cherry Girl.",
+    "Not gonna lie. I liked doing that.",
+}
